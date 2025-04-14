f4b58c71b0bef149b840114271c671996bb5116e
Janna Joyce E. Jumao-as <jannajoyce.jumao-as@g.msuiit.edu.ph>
2025-04-14 14:31:51 +0800

    Add files via upload

---
+++ lab1_jumao-as/gwapako.asm
@@ -0,0 +1,24 @@
+# Program Name: HelloWorld.asm	(Optional)
+
+
+
+	.data				# data segment
+
+hello:	.asciiz "Hello, world!\n"	# a null terminated string
+
+	.text				# code segment
+
+	.globl main			#
+
+main:
+
+	la	$a0, hello		# load string address
+
+	li	$v0, 4			# specify system write service
+
+	syscall				# call the kernel (write string)
+
+	li	$v0, 10			# exit to OS
+
+	syscall
+
